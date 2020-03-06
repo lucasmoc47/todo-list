dc9b897ee534e73fd80dbd00f1bb0d83c4c32074
Lucas <lucasmoc47@gmail.com>
2020-03-06 19:59:21 -0300

    Adding the first action to the todosReducer

--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -1,5 +1,16 @@
-const reducer = state => {
-	return state
+import { v4 as uuidv4 } from 'uuid'
+import { ADD_TODO } from '../constants/actions'
+
+
+const reducer = (state, action) => {
+	const newTodo = { id: uuidv4(), task: action.task, completed: false }
+
+	switch(action.type) {
+		case ADD_TODO:
+			return [...state, newTodo]
+		default:
+			return state
+	}
 }
 
 export default reducer
