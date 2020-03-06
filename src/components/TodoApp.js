5551ee71bb7b8cf26f9bf45342617433e9b61179
Lucas <lucasmoc47@gmail.com>
2020-03-06 19:56:20 -0300

    Creating and importing TodoForm

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { TodosProvider } from '../contexts/TodosContext'
 
 import TodoList from './TodoList'
+import TodoForm from './TodoForm'
 
 // import { Container } from './styles';
 
 export default function TodoApp() {
 	return (
 		<TodosProvider>
+			<TodoForm />
 			<TodoList />
 		</TodosProvider>
 	);
