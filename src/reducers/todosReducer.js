d7f1f3dea3fd2584783b3271559b66a77a2e9d63
Lucas <lucasmoc47@gmail.com>
2020-03-06 20:26:52 -0300

    Adding REMOVE_TODO and TOGGLE_TODO functionalities to the reducer

--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -1,13 +1,17 @@
 import { v4 as uuidv4 } from 'uuid'
-import { ADD_TODO } from '../constants/actions'
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from '../constants/actions'
 
 
 const reducer = (state, action) => {
-	const newTodo = { id: uuidv4(), task: action.task, completed: false }
-
 	switch(action.type) {
 		case ADD_TODO:
-			return [...state, newTodo]
+			return [...state, { id: uuidv4(), task: action.task, completed: false }]
+		case REMOVE_TODO:
+			return state.filter(todo => todo.id !== action.id)
+		case TOGGLE_TODO:
+			return state.map(todo => 
+				todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+			)
 		default:
 			return state
 	}
