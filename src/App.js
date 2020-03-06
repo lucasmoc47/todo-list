49d91111cde953a967e19fa09e1ff6f104b1bfa0
Lucas <lucasmoc47@gmail.com>
2020-03-06 19:20:43 -0300

    Adding a header to App.js and styling it

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+	return (
+		<div>
+			<header className="header">
+				<h1>
+					todo<span>list</span>
+				</h1>
+				<h2>A simple todolist app built with React Hooks & Context</h2>
+			</header>
+   		</div>
+	);
 }
 
 export default App;
