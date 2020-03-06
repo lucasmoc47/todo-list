345b155cc04f5c91fe1862fc4753ac8b2a8745a4
Lucas <lucasmoc47@gmail.com>
2020-03-06 19:44:37 -0300

    Creating TodoList component

---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { TodosContext } from '../contexts/TodosContext'
+
+// import { Container } from './styles';
+
+export default function TodoList() {
+	const todos = useContext(TodosContext)
+
+	return (
+		<ul>
+			{todos.map(todo => (
+				<li key={todo.id}>{todo.task}</li>
+			))}
+		</ul>
+	);
+}
